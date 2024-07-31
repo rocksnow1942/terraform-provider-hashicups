f88dc943487969ea43bb4d77713745e6a983f332
Hui Kang <huikang@google.com>
2024-07-31 14:42:55 -0700

    update

---
+++ examples/provider-install-verification/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    hashicups = {
+      source = "hashicorp.com/edu/hashicups"
+    }
+  }
+}
+
+provider "hashicups" {
+
+} 
+
+data "hashicups_coffees" "example" {
+
+}
